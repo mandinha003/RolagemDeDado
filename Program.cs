1c4c530c80501ff30c6cae8fadede39083c66611
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-05 21:43:40 -0300

    terminei

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
 ﻿using System.Security.Cryptography;
+int faces, sorteado;
 
 Console.WriteLine("--- Rolagem de Dados --- \n");
-Console.WriteLine(" Quantidade de Faces: 20");
-Console.Write(" Rolagem: ");
-Console.Write(RandomNumberGenerator.GetInt32(1, 21));
+
+Console.Write(" Quantidade de Faces? ");
+faces = Convert.ToInt32(Console.ReadLine());
+sorteado = RandomNumberGenerator.GetInt32(1, faces + 1);
+
+Console.Write($"\n Rolagem {sorteado}");
+
+
 
 
